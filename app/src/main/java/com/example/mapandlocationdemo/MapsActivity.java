6443156f395cc3175e4c0439d6955f7b862ebf89
Ayaz Khan <97739190+ak086@users.noreply.github.com>
2023-01-02 16:56:36 +0530

    Use of geocode

--- app/src/main/java/com/example/mapandlocationdemo/MapsActivity.java
+++ app/src/main/java/com/example/mapandlocationdemo/MapsActivity.java
@@ -8,11 +8,16 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,6 +27,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.mapandlocationdemo.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -37,7 +46,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if(requestCode==1){
             if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
                 if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,1,locationListener);
                 }
             }
         }
@@ -74,27 +83,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 mMap.clear();
                 LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Your location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,10));
+
+                Geocoder geocoder=new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    //list view is to store the best address which we found through get from location and here max result is best match which we can found
+                    //from the lat and lon.
+                    List<Address> addressList=geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+
+                    if(addressList!=null && addressList.size()>0){
+
+                        Log.i("Place Info", addressList.get(0).toString());
+                        /*this is simple log in which we got lot of information which contains
+                        address line,sub-admin,admin,postal code,country code. So now we are going to use
+                        this information */
+
+                        String address="";
+                        if(addressList.get(0).getAdminArea()!=null){
+                            address+=addressList.get(0).getAdminArea()+" ";
+                        }
+                        if(addressList.get(0).getCountryCode()!=null){
+                            address+=addressList.get(0).getCountryCode()+" ";
+                        }
+                        if(addressList.get(0).getLocality()!=null){
+                            address+=addressList.get(0).getLocality()+" ";
+                        }
+                        if(addressList.get(0).getCountryName()!=null){
+                            address+=addressList.get(0).getCountryCode()+" ";
+                        }
+                        Toast.makeText(MapsActivity.this,address,Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
         };
 
-
-
-
         if(Build.VERSION.SDK_INT<23){
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,1,locationListener);
         }else{
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
                 ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
             }else{
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,1,locationListener);
                 Location lastKnownLoaction=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
                 mMap.clear();
                 LatLng sydney = new LatLng(lastKnownLoaction.getLatitude(), lastKnownLoaction.getLongitude());
-                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Your Location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,10));
             }
         }
